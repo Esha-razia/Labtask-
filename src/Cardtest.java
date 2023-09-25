7f21e04e23ee3541738e726853248c20eaa79e48
Esha <esharazia2002@gmail.com>
2023-09-25 05:29:26 -0700

    file added

--- src/Cardtest.java
+++ src/Cardtest.java
@@ -1,10 +1,11 @@
 public class Cardtest {
     public static void main(String[] args) {
         Deck d1 = new Deck();
-
+//tp print
         System.out.println(d1);
         Game game= new Game(d1);
         System.out.println(game);
+
 
     }
 }
